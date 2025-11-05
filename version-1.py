757128e5af7ea16ccbff4d7c5f351932eb17bf26
fatoumata-fofana <ftimtim17@gmail.com>
2025-11-05 11:14:12 -0500

    version-1.py

--- version-1.py
+++ version-1.py
@@ -1,1 +1,9 @@
+import random
 
+random_number = random.randint(1, 10)
+user_guess = int(input("guess a number between 1 and 10:"))
+if user_guess == random_number:
+  print ("you win! you guessed the correct number")
+else: 
+  print("you lose!")
+  
