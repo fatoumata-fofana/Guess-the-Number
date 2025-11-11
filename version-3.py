aa7529a01bdeca652eb3bc5d6d968806a68d5855
fatoumata-fofana <ftimtim17@gmail.com>
2025-11-11 11:38:18 -0500

    version-3.py

--- version-3.py
+++ version-3.py
@@ -1,1 +1,18 @@
+import random
+
+limite = 6
+random_number = random.randint(1, 10)
+print(random_number)
+guessed = False
+user_guess = int(input("Enter a number between 1 and 10:"))
+limite -= 1
+ 
+while user_guess != random_number and limite > 1:
+  user_guess =int(input("Enter a number between 1 and 10:"))
+  limite -= 1
+  if user_guess == random_number: 
+      print("you win!")
+      guessed = True
+  else:
+      print("you lose!")
 
